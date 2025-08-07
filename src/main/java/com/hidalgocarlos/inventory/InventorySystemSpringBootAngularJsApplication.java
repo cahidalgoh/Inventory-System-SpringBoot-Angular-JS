2424afba1f7e69a53e3e384e97e20a97133f8965
Carlos Hidalgo <carlos.hidalgoh@hotmail.com>
2025-08-07 11:40:25 -0500

    Initial commit, project generated

---
+++ src/main/java/com/hidalgocarlos/inventory/InventorySystemSpringBootAngularJsApplication.java
@@ -0,0 +1,13 @@
+package com.hidalgocarlos.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySystemSpringBootAngularJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySystemSpringBootAngularJsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hidalgocarlos/inventory/InventorySystemSpringBootAngularJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hidalgocarlos.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySystemSpringBootAngularJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
