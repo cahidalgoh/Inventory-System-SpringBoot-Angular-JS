ac794f7f9c410bdc26073bc84f0d3ca70f6e3d1f
Carlos Hidalgo <carlos.hidalgoh@hotmail.com>
2025-08-08 02:57:13 -0500

    feat(utils): add ErrorResponse class for standardized error handling

---
+++ src/main/java/com/hidalgocarlos/inventory/utils/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.hidalgocarlos.inventory.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String message;
+    private Integer code;
+
+
+
+}
