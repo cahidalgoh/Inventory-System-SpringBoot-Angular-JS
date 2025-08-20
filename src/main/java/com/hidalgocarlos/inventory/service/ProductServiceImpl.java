e48c2c43e433c693699f6e15ec68ad9b503a35ea
Carlos Hidalgo <carlos.hidalgoh@hotmail.com>
2025-08-20 15:56:34 -0500

    refactor(service): retornar Product en saveOrUpdateProduct de ProductServiceImpl
    Se actualizó la implementación de saveOrUpdateProduct para que retorne el producto guardado o actualizado. Esto permite acceder a información del producto persistido, como su ID generado automáticamente.

--- src/main/java/com/hidalgocarlos/inventory/service/ProductServiceImpl.java
+++ src/main/java/com/hidalgocarlos/inventory/service/ProductServiceImpl.java
@@ -32,8 +32,8 @@ public class ProductServiceImpl implements IProductService {
     }
 
     @Override
-    public void saveOrUpdateProduct(Product product) {
-        this.productRepository.save(product);
+    public Product saveOrUpdateProduct(Product product) {
+        return this.productRepository.save(product);
     }
 
     @Override
